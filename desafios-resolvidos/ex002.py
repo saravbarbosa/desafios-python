fd7f3dbe02b25326342f47e51842228732aee13a
Sara Barbosa <97530586+saravbarbosa@users.noreply.github.com>
2022-11-01 22:20:25 -0300

    Create ex002.py

---
+++ desafios-resolvidos/ex002.py
@@ -0,0 +1,9 @@
+#1
+nome = input("Digite o seu nome: ")
+print(f"É um prazer te conhecer, {nome}!")
+
+#2
+print("É um prazer te conhecer,", nome)
+
+#3
+print("É um prazer te conhecer, {}!".format(nome))
