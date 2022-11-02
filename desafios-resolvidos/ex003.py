97c87420142fa10f67d8251391f54d46b208e18f
Sara Barbosa <97530586+saravbarbosa@users.noreply.github.com>
2022-11-01 22:31:26 -0300

    Update ex003.py

--- desafios-resolvidos/ex003.py
+++ desafios-resolvidos/ex003.py
@@ -1,5 +1,5 @@
-n1 = int(input("Digite um número: "))
-n2 = int(input("Digite outro número: "))
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
 
 soma = n1 + n2
 print(f"A soma entre {n1} + {n2} é {soma}.")
